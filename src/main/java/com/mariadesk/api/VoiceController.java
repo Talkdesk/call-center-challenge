5c65840ebbc1bf123e7ad41d37dbf920794ce02d
Jorge Martins <jorgemlm@gmail.com>
2018-02-05 22:02:24 +0000

    Set the base codebase for the challenge

--- src/main/java/com/mariadesk/api/VoiceController.java
+++ src/main/java/com/mariadesk/api/VoiceController.java
@@ -1,26 +1,12 @@
 package com.mariadesk.api;
 
-import com.mariadesk.models.Call;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import com.twilio.twiml.VoiceResponse;
-
-import com.mariadesk.api.responses.Inbound;
-import com.mariadesk.api.responses.IVR;
-import com.mariadesk.api.responses.Questions;
-import com.mariadesk.repositories.CallsRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VoiceController {
-
-  private CallsRepository callsRepository;
-
-  @Autowired
-  public VoiceController(CallsRepository callsRepository){
-    this.callsRepository = callsRepository;
-  }
 
   @RequestMapping(path="/voice/inbound", produces="application/xml")
   public VoiceResponse inbound(@RequestParam("From") String from,
@@ -28,24 +14,16 @@ public class VoiceController {
                                @RequestParam("Caller") String caller,
                                @RequestParam("CallSid") String callId,
                                @RequestParam("AccountSid") String accountId) {
-    Call call = new Call();
-    call.setAccountId(accountId);
-    call.setExternalCallId(callId);
-    call.setFrom(from);
-    call.setTo(to);
-
-    this.callsRepository.save(call);
-
-    return new Inbound().response();
+    throw new UnsupportedOperationException("To be implemented");
   }
 
   @RequestMapping(path="/voice/ivr", produces="application/xml")
   public VoiceResponse ivr(@RequestParam(value="Digits", defaultValue="") String digits) {
-    return new IVR(digits).response();
+    throw new UnsupportedOperationException("To be implemented");
   }
 
   @RequestMapping(path="/voice/questions", produces="application/xml")
   public VoiceResponse questions(@RequestParam(value="Digits", defaultValue="") String digits) {
-    return new Questions(digits).response();
+    throw new UnsupportedOperationException("To be implemented");
   }
 }
